4023ef9517eef776886526e38fda923ead0fe9b4
Arthur <liangw6@cs.washington.edu>
2020-03-19 02:26:02 -0700

    Finalized on data and figures

--- eval.py
+++ eval.py
@@ -6,6 +6,7 @@ import pickle
 GHOSTERY_DATA_FILE_PATH = "ghostery.csv"
 FIREFOX_DATA_FILE_PATH = "firefox.csv"
 PRIVACY_BADGER_DATA_FILE_PATH = "privacy_badger.csv"
+UBLOCK_FILE_PATH = "ublock.csv"
 
 '''
 Takes a comma separated file of websites and observed trackers in the form of:
@@ -112,14 +113,16 @@ if __name__ == '__main__':
     eval.process_data(GHOSTERY_DATA_FILE_PATH, "Ghostery")
     eval.process_data(FIREFOX_DATA_FILE_PATH, "Firefox")
     eval.process_data(PRIVACY_BADGER_DATA_FILE_PATH, "PrivacyBadger")
+    eval.process_data(UBLOCK_FILE_PATH, "uBlock")
 
     # UNCOMMENT THIS LINE TO SEE EVALUATION FUNCTION THAT PLACES HIGHER IMPORTANCE ON BLOCKING OBSCURE TRACKERS
     # eval.set_evaluation_function(alt_evaluation_function)
 
-    print("Blockers\tGhostery\tFirefox\t\tPrivacyBadger")
-    print("All websites\t{:.3f}\t\t{:.3f}\t\t{:.3f}".format(eval.blocker_score("Ghostery"),
+    print("Blockers\tGhostery\tFirefox\t\tPrivacyBadger\tuBlock")
+    print("All websites\t{:.3f}\t\t{:.3f}\t\t{:.3f}\t{:.3f}".format(eval.blocker_score("Ghostery"),
                                                              eval.blocker_score("Firefox"),
-                                                             eval.blocker_score("PrivacyBadger")))
+                                                             eval.blocker_score("PrivacyBadger"),
+                                                             eval.blocker_score("uBlock")))
     
     # To evaluate each blocker on a subset of the collected websites, simply create a subset of websites that should be
     # considered i.e. subset = set(); subset.add(NEWS_WEBSITE); ... and call eval.blocker_subset_score("Ghostery", subset) etc.
@@ -130,11 +133,12 @@ if __name__ == '__main__':
         subset = set(website_by_type[site_category])
         if (len(site_category) < 5):
             site_category = site_category + "\t"
-        print("{}\t{:.3f}\t\t{:.3f}\t\t{:.3f}".format(
+        print("{}\t{:.3f}\t\t{:.3f}\t\t{:.3f}\t{:.3f}".format(
             site_category,
             eval.blocker_subset_score("Ghostery", subset),
             eval.blocker_subset_score("Firefox", subset),
-            eval.blocker_subset_score("PrivacyBadger", subset)))
+            eval.blocker_subset_score("PrivacyBadger", subset),
+            eval.blocker_subset_score("uBlock", subset)))
 
     # Uncomment this line to save each website and its corresponding tracker informaiton into a pickle file
     # eval.save_website_trackers_to_pickle('web2tracker.pkl')
